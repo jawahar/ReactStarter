596363fa5085f35f22bdfd9d5b0516d4a3b2ad8e
Jawahar Malhotra <jawahar.malhotra@gmail.com>
2016-07-31 16:45:31 +0530

    lesson 45 complete; Header component using React Router's Link to navigate

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,10 +1,14 @@
 var React = require('react');
+var Header = require('./header');
+
+var renderCount = 0;
 
 module.exports = React.createClass({
   render: function() {
+    console.log(renderCount++);
     return <div>
-    I am a header
-    {this.props.children}
+      <Header />
+      {this.props.children}
     </div>
   }
-})
+});
