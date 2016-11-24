24b33cc7f84c9e9dd694466d35d1525ed405d20b
Jawahar Malhotra <jawahar.malhotra@gmail.com>
2016-11-23 16:38:46 -0800

    page looks decent wth styles

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,13 +1,19 @@
 var Fetch = require('whatwg-fetch');
-var rootUrl = 'https://api.imgur.com/3';
+var rootUrl = 'https://api.imgur.com/3/';
 var apiKey = '37653ec83efe0dd';
 
-module.exports = {
+module.exports = window.api = {
   get: function(url) {
     return fetch(rootUrl + url, {
       headers: {
         'Authorization': 'Client-ID ' + apiKey
       }
+    })
+    .then(function(response){
+      return (response.json());
+    })
+    .then(function(data){
+      console.log(data);
     })
   }
 }
